78f9247a56cf2c7690229d90bd88add9c86885d3
suiyujin <suiyujin04@gmail.com>
2015-05-31 05:32:56 +0900

    change column default of profile_image_path from users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150530201105) do
+ActiveRecord::Schema.define(version: 20150530203028) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title",         limit: 255
@@ -85,20 +85,20 @@ ActiveRecord::Schema.define(version: 20150530201105) do
   add_index "hobby_users", ["user_id"], name: "index_hobby_users_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  limit: 255, default: "",                     null: false
-    t.string   "encrypted_password",     limit: 255, default: "",                     null: false
+    t.string   "email",                  limit: 255, default: "",                                                      null: false
+    t.string   "encrypted_password",     limit: 255, default: "",                                                      null: false
     t.string   "reset_password_token",   limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          limit: 4,   default: 0,                      null: false
+    t.integer  "sign_in_count",          limit: 4,   default: 0,                                                       null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip",     limit: 255
     t.string   "last_sign_in_ip",        limit: 255
-    t.datetime "created_at",                                                          null: false
-    t.datetime "updated_at",                                                          null: false
-    t.string   "name",                   limit: 255, default: "名称未設定",                null: false
-    t.string   "profile_image_path",     limit: 255, default: "image/dummy_user.jpg"
+    t.datetime "created_at",                                                                                           null: false
+    t.datetime "updated_at",                                                                                           null: false
+    t.string   "name",                   limit: 255, default: "名称未設定",                                                 null: false
+    t.string   "profile_image_path",     limit: 255, default: "http://otakku-no1.herokuapp.com/images/dummy_user.jpg"
     t.string   "profile",                limit: 255
     t.float    "buttle_points",          limit: 24,  default: 0.0
   end
