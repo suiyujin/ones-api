889a8221341dbc0f0563dc6ba91889797b39ac8c
suiyujin <suiyujin04@gmail.com>
2015-05-30 15:35:23 +0900

    fix association hobby and user

--- app/models/hobby.rb
+++ app/models/hobby.rb
@@ -1,5 +1,6 @@
 class Hobby < ActiveRecord::Base
   belongs_to :category
-  has_many :articles, :hobby_users
+  has_many :articles
+  has_many :hobby_users
   has_many :users, :through => :hobby_users
 end
