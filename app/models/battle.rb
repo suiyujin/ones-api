caa482cbff5306a7f554831352cdaf7472f0a7df
tommy96 <tomomo96@gmail.com>
2015-05-31 06:01:52 +0900

    modify_battle.rb

--- app/models/battle.rb
+++ app/models/battle.rb
@@ -1,4 +1,4 @@
 class Battle < ActiveRecord::Base
-  belongs_to :article1_id, :class_name => 'Article'
-  belongs_to :article2_id, :class_name => 'Article'
+  belongs_to :article1, :class_name => 'Article'
+  belongs_to :article2, :class_name => 'Article'
 end
