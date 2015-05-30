cb3c6ff84f04fca344a9fb5b43ebaad2b8bffcfd
suiyujin <suiyujin04@gmail.com>
2015-05-30 14:13:15 +0900

    add name, profile_image_path and profile to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,21 +11,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150530044637) do
+ActiveRecord::Schema.define(version: 20150530045656) do
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  limit: 255, default: "", null: false
-    t.string   "encrypted_password",     limit: 255, default: "", null: false
+    t.string   "email",                  limit: 255, default: "",                     null: false
+    t.string   "encrypted_password",     limit: 255, default: "",                     null: false
     t.string   "reset_password_token",   limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          limit: 4,   default: 0,  null: false
+    t.integer  "sign_in_count",          limit: 4,   default: 0,                      null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip",     limit: 255
     t.string   "last_sign_in_ip",        limit: 255
-    t.datetime "created_at",                                      null: false
-    t.datetime "updated_at",                                      null: false
+    t.datetime "created_at",                                                          null: false
+    t.datetime "updated_at",                                                          null: false
+    t.string   "name",                   limit: 255, default: "名称未設定",                null: false
+    t.string   "profile_image_path",     limit: 255, default: "image/dummy_user.jpg"
+    t.string   "profile",                limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
