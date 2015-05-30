a942a3ba48bf71ebc83bb6031bc933e14137ff06
suiyujin <suiyujin04@gmail.com>
2015-05-31 04:42:03 +0900

    get timeline

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -68,6 +68,63 @@ class UsersController < ApplicationController
     render json: res
   end
 
+  def getTimeLine
+    login_user_id = params[:my_id]
+    login_user = User.find(login_user_id)
+
+    limit_num = 20
+
+    follow_users = login_user.follows_of_from_user
+
+    follow_users_articles = Array.new
+    follow_users.each do |follow_user|
+      follow_users_articles << make_articles_hash(follow_user.articles, follow_user)
+    end
+    follow_users_articles.flatten!
+
+    articles_new = follow_users_articles.sort do |a, b|
+      a['created_at'] <=> b['created_at']
+    end.reverse.shift(limit_num)
+
+    follow_users_articles_hot = follow_users_articles.select do |follow_users_article|
+      follow_users_article['created_at'] >= 1.weeks.ago
+    end
+    articles_hot = follow_users_articles_hot.sort do |a, b|
+      a['point'] <=> b['point']
+    end.reverse.shift(limit_num)
+
+    articles_legend = follow_users_articles.sort do |a, b|
+      a['point'] <=> b['point']
+    end.reverse.shift(limit_num)
+
+    if follow_users_articles.blank?
+      res = {
+        result: false,
+        data: nil
+      }
+    else
+      res = {
+        result: true,
+        data: [
+          {
+            segment_type: 'new',
+            articles: articles_new
+          },
+          {
+            segment_type: 'hot',
+            articles: articles_hot
+          },
+          {
+            segment_type: 'legend',
+            articles: articles_legend
+          }
+        ]
+      }
+    end
+
+    render json: res
+  end
+
   def follow
     Follow.find_or_create_by(from_user_id: params[:my_id],to_user_id: params[:target_id])
   end
