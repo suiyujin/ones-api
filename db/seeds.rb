81c1c5727bff2a4dbcb322da6b16e725ad532afa
suiyujin <suiyujin04@gmail.com>
2015-05-31 05:29:23 +0900

    fix seed data of users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,11 @@
 require 'csv'
 
-CSV.foreach('db/testcsv/users.csv') do |row|
+CSV.foreach('db/testcsv/users_unix.csv') do |row|
   User.create(email:row[1],
               password:row[2],
               password_confirmation:row[3],
-              name:row[12])
+              name:row[12],
+              profile_image_path:row[13])
 end
 
 CSV.foreach('db/testcsv/follows.csv') do |row|
